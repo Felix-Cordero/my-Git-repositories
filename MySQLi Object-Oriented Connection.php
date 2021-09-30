e1983e6a621ff6cda07f51632c62765f8cbe962c
Felix Frederick <corderofelix101@gmail.com>
2021-09-30 19:10:39 +0800

    Create MySQLi Object-Oriented Connection.php

---
+++ MySQLi Object-Oriented Connection.php
@@ -0,0 +1,48 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+<?php
+
+/*
+
+// Note on the object-oriented example above:
+
+$connect_error was broken until PHP 5.2.9 and 5.3.0. If you need to ensure compatibility with PHP versions prior to 5.2.9 and 5.3.0, use the following code instead:
+
+// Check connection
+if (mysqli_connect_error()) {
+  die("Database connection failed: " . mysqli_connect_error());
+}
+
+*/
+
+
+
+  /* Take note by Felix Cordero Jr.
+
+    // MySQLi Object-Oriented:
+
+       $conn->close();
+
+    // MySQLi Procedural:
+
+       mysqli_close($conn);
+
+    // PDO (PHP Data Objects)
+
+       $conn = null;
+  */
+
+?>
